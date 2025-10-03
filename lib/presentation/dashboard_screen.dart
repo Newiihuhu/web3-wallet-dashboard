3b54ff8d02f80f8b2aa112f09134cf4857881ac0
Newiihuhu <newiihuhu@gmail.com>
2025-10-03 18:04:26 +0700

    Refactor DashboardScreen to use BlocBuilder for state management

--- lib/presentation/dashboard_screen.dart
+++ lib/presentation/dashboard_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:web3_wallet_dashboard/common/injection/service_locator.dart';
 import 'package:web3_wallet_dashboard/presentation/bloc/dashboard_bloc.dart';
-import 'package:web3_wallet_dashboard/presentation/widgets/wallet_overview_widget.dart';
+import 'package:web3_wallet_dashboard/presentation/bloc/dashboard_event.dart';
+import 'package:web3_wallet_dashboard/presentation/bloc/dashboard_state.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -13,45 +14,200 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  late final DashboardBloc _dashboardBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _dashboardBloc = getIt<DashboardBloc>();
+    _dashboardBloc.add(
+      const GetEthBalanceEvent(
+        address: '0x86bBF3f5B7fd6bB206f0070fAcF88556aB905088',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt<DashboardBloc>(),
+    return BlocProvider.value(
+      value: _dashboardBloc,
       child: Scaffold(
+        backgroundColor: Colors.grey[200],
         appBar: AppBar(
-          title: Text('Wallet Dashboard'),
-          backgroundColor: const Color.fromARGB(255, 175, 175, 175),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Wallet Dashboard'),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors
+                          .green, // You can change this based on network status
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Text(
+                    'Sepolia Testnet',
+                    style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          backgroundColor: Colors.grey[300],
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text(
-                  'Address: ',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Text(
-                  shortenAddress('0x86bBF3f5B7fd6bB206f0070fAcF88556aB905088'),
-                ),
-                IconButton(
-                  onPressed: () {
-                    Clipboard.setData(
-                      ClipboardData(
-                        text: '0x86bBF3f5B7fd6bB206f0070fAcF88556aB905088',
+        body: BlocBuilder<DashboardBloc, DashboardState>(
+          builder: (context, state) {
+            if (state is DashboardLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is DashboardLoaded) {
+              return Container(
+                margin: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      // decoration: BoxDecoration(
+                      //   borderRadius: BorderRadius.circular(8),
+                      //   border: Border.all(color: Colors.blueGrey[200]!),
+                      //   color: Colors.grey[50],
+                      // ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Address',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                shortenAddress(
+                                  '0x86bBF3f5B7fd6bB206f0070fAcF88556aB905088',
+                                ),
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              const SizedBox(width: 8),
+                              IconButton(
+                                onPressed: () {
+                                  Clipboard.setData(
+                                    ClipboardData(
+                                      text:
+                                          '0x86bBF3f5B7fd6bB206f0070fAcF88556aB905088',
+                                    ),
+                                  );
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        'Address copied to clipboard',
+                                      ),
+                                    ),
+                                  );
+                                },
+                                icon: Icon(
+                                  Icons.copy,
+                                  color: Colors.blueGrey[500],
+                                  size: 16,
+                                ),
+                              ),
+                              // Icon(
+                              //   Icons.refresh,
+                              //   color: Colors.blueGrey[500],
+                              //   size: 16,
+                              // ),
+                              // const SizedBox(width: 4),
+                              // Text(
+                              //   'Refresh',
+                              //   style: TextStyle(
+                              //     fontSize: 14,
+                              //     color: Colors.blueGrey[500],
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                          // const SizedBox(height: 8),
+                          // Row(
+                          //   children: [
+                          //     Text(
+                          //       'Last sync: 10/01/2025 10:00:00',
+                          //       style: TextStyle(
+                          //         fontSize: 12,
+                          //         color: Colors.grey[600],
+                          //       ),
+                          //     ),
+                          //   ],
+                          // ),
+                        ],
                       ),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Address copied to clipboard')),
-                    );
-                  },
-                  icon: Icon(Icons.copy, color: Colors.blueGrey[500], size: 16),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.grey[200]!),
+                            color: Colors.grey[50],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Wallet Overview',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              const SizedBox(height: 8),
+                              const Text(
+                                'Balances',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'ETH:',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(state.ethBalance.balanceETH),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Text('Last sync: 10/01/2025 10:00:00'),
-            WalletOverviewWidget(),
-          ],
+              );
+            }
+            return const SizedBox.shrink();
+          },
         ),
       ),
     );
